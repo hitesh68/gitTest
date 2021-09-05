4f730e9cf412e8b2dff427f7441074e076bef81e
hitesh68 <hiteshpoddar882@gmail.com>
2021-09-05 12:31:02 +0530

    testing session module

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-const express = requiure('express')
-console.log(express)
+const express = require('express')
+const app = express()
+const session = require('express-session')
+
+app.use(session({
+    secret : 'test'
+}))
+
+const port = 5000
+app.listen(port,() => {
+    console.log("Server is up at "+port)
+})
+// console.log(express)
